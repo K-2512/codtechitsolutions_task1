e80dd42625e93cde6d60b7a9e02e78a35b427218
K-2512 <ykalyani.2512@gmail.com>
2024-06-09 17:06:07 +0530

    Add files via upload

---
+++ src/simplejavacalculator/SimpleJavaCalculator.java
@@ -0,0 +1,17 @@
+
+
+package simplejavacalculator;
+
+public class SimpleJavaCalculator {
+   
+   public static void main(String[] args) {
+      try {
+         UI uiCal = new UI();
+         uiCal.init();
+      }
+      catch (Exception e) {
+         System.out.println(e.getMessage());   
+      }
+      
+   }
+}
